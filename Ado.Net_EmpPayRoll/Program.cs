8b1f95c2e0eb6843b23c3c97b0dd30ebcbb67d98
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-02-16 22:15:14 +0530

    [Sartaj] : added Connect to database program

---
+++ Ado.Net_EmpPayRoll/Program.cs
@@ -0,0 +1,14 @@
+﻿using Ado.Net_EmpPayRoll;
+
+namespace Ado.Net_EmpPayRoll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeRepo repo = new EmployeeRepo();
+            repo.GetAllEmployees();
+            Console.ReadLine();
+        }
+    }
+}
